f96ff38f861e0527f841efc2b7c5df24faae9ed9
nGin482 <ngin2547@gmail.com>
2021-08-08 18:20:28 +1000

    Filter to control what 'service' should be displayed

--- src/TVGuide.js
+++ src/TVGuide.js
@@ -6,6 +6,7 @@ import './TVGuide.css';
 const TVGuide = () => {
     const [guide, setGuide] = useState(null)
     const [message, setMessage] = useState('')
+    const [service, setService] = useState('All')
     // const history = useHistory()
     
     useEffect(() => {
@@ -40,22 +41,34 @@ const TVGuide = () => {
     }
 
     const renderGuideData = () => {
-        return (
-            <div id="guide-data">
-                <div className="service" id="Free to Air">
-                    <h6 className="service-header">Free to Air</h6>
-                    {guide['FTA'].map(show => (
+        if (service === 'All') {
+            return (
+                <div id="guide-data">
+                    <div className="service" id="Free to Air">
+                        <h6 className="service-header">Free to Air</h6>
+                        {guide['FTA'].map(show => (
+                            <blockquote className="show" key={show.time}>{showDetails(show)}</blockquote>
+                        ))}
+                    </div>
+                    <div className="service" id="BBC">
+                        <h6 className="service-header">BBC Channels</h6>
+                        {guide['BBC'].map(show => (
+                            <blockquote className="show" key={show.time}>{showDetails(show)}</blockquote>
+                        ))}
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div className="service" id={service}>
+                    {service === 'FTA' ? <h6 className="service-header">Free to Air</h6> : <h6 className="service-header">BBC Channels</h6>}
+                    {guide[service].map(show => (
                         <blockquote className="show" key={show.time}>{showDetails(show)}</blockquote>
                     ))}
                 </div>
-                <div className="service" id="BBC">
-                    <h6 className="service-header">BBC Channels</h6>
-                    {guide['BBC'].map(show => (
-                        <blockquote className="show" key={show.time}>{showDetails(show)}</blockquote>
-                    ))}
-                </div>
-            </div>
-        )
+            )
+        }
     }
 
     // const handleClick = () => {
@@ -84,6 +97,17 @@ const TVGuide = () => {
         <div id="tv-guide">
             <h4 id="tv-guide-header">Guide</h4>
             {display()}
+            <div id="service-filter">
+                <div className="select-service" id="select-fta" onClick={() => setService('FTA')}>
+                    Free to Air
+                </div>
+                <div className="select-service" id="select-bbc" onClick={() => setService('BBC')}>
+                    BBC Channels
+                </div>
+                <div className="select-service" id="select-bbc" onClick={() => setService('All')}>
+                    All
+                </div>
+            </div>
         </div>
     )
 }
