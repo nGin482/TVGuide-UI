b6d5130931831e2db31e394690b859da20da4db7
nGin482 <ngin2547@gmail.com>
2021-08-09 22:54:17 +1000

    ShowListPage component now used when the route changes

--- src/Navigation/NavigationHandle.js
+++ src/Navigation/NavigationHandle.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavigationMenu from "./NavigationMenu.js";
 import Home from "../Home.js";
-import ShowList from "../ShowList.js";
+import ShowListPage from "../ShowListPage.js";
 
 const NavigationHandle = () => {
     return (
         <BrowserRouter>
             <NavigationMenu/>
             <Switch>
-                <Route path='/show-list'><ShowList/></Route>
+                <Route path='/show-list'><ShowListPage/></Route>
                 <Route path='/shows'></Route>
                 <Route path='/reminders'></Route>
                 <Route path='/'><Home/></Route>
---
+++ src/ShowListPage.js
@@ -0,0 +1,11 @@
+import ShowList from "./ShowList";
+import './ShowList.css';
+
+const ShowListPage = () => (
+    <div id="show-list-page">
+        <h1>List of Shows</h1>
+        <ShowList/>
+    </div>
+)
+
+export default ShowListPage;
