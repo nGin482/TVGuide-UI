7846325a3674493fbc8f5f39abc7a3592d411a69
nGin482 <ngin2547@gmail.com>
2021-08-09 19:13:50 +1000

    GET request to retrieve the list of shows searched

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const baseURL = 'http://127.0.0.1:5000'
+const baseURL = 'http://192.168.1.120:5000'
 
 const getGuide = () => {
     return axios.get(baseURL + '/guide').then(response => response.data)
 }
 
+const getShowList = () => {
+    return axios.get(baseURL + '/show-list').then(response => response.data)
+}
+
 const requests = {
-    getGuide
+    getGuide,
+    getShowList
 }
 
 export default requests;
