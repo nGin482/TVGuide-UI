d49993f4fbeef59d327321d1b68c464d6d9ea340
nGin482 <ngin2547@gmail.com>
2021-08-10 00:08:05 +1000

    GET request to retrieve the events of DB interactions

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -1,18 +1,23 @@
 import axios from "axios";
 
-const baseURL = 'http://192.168.1.120:5000'
+const baseURL = 'http://192.168.1.120:5000/'
 
 const getGuide = () => {
-    return axios.get(baseURL + '/guide').then(response => response.data)
+    return axios.get(baseURL + 'guide').then(response => response.data)
+}
+
+const getEvents = () => {
+    return axios.get(baseURL + 'events').then(response => response.data)
 }
 
 const getShowList = () => {
-    return axios.get(baseURL + '/show-list').then(response => response.data)
+    return axios.get(baseURL + 'show-list').then(response => response.data)
 }
 
 const requests = {
     getGuide,
-    getShowList
+    getShowList,
+    getEvents
 }
 
 export default requests;
