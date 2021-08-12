dc416a50fa99fa67e222cba9b5c51174bd58e042
nGin482 <ngin2547@gmail.com>
2021-08-12 19:11:53 +1000

    Adding the event to the show if the lengths are the same
    Wait for data to be retrieved before rendering it

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
     const [events, setEvents] = useState([])
     const [guideError, setGuideError] = useState('')
     const [eventsError, setEventsError] = useState('')
+    const [showEvents, setShowEvents] = useState(true)
 
     useEffect(() => {
         requests.getGuide().then(data => {
@@ -32,13 +33,41 @@ const Home = () => {
     }, []
     )
 
-    return (
+    const linkGuidetoEpisode = () => {
+        const filteredFTAGuide = guide['FTA'].filter(show => show.channel !== 'ABCHD')
+        const filteredGuide = filteredFTAGuide.concat(guide['BBC'])
+
+        if (filteredGuide.length === events.length) {
+            filteredGuide.forEach((show, index) => {
+                show.event = events[index]
+            });
+        }
+        else {
+            return 'Unable to link the shows to the events.'
+        }
+        console.log(filteredGuide)
+
+    }
+
+    if (guide && events) {
+        return (
+            <div id="home">
+                <h1>TV Guide</h1>
+                <TVGuide guide={guide} message={guideError}/>
+                {linkGuidetoEpisode()}
+                {showEvents ? <button onClick={() => setShowEvents(false)}>Hide Events</button> : <button onClick={() => setShowEvents(true)}>Show Events</button>}
+                {showEvents ? <Events events={events} error={eventsError}/> : ''}
+            </div>
+        )
+    }
+    else {
+        return (
         <div id="home">
-            <h1>TV Guide</h1>
-            <TVGuide guide={guide} message={guideError}/>
-            <Events events={events} error={eventsError}/>
+            <h1>TVGuide</h1>
+            Waiting for data to be retrieved ...
         </div>
-    )
+        )
+    }
 }
 
 export default Home;
