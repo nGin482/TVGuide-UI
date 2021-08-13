f2ef8fed68e4a827de76d5c33657eabe17bd6fdc
nGin482 <ngin2547@gmail.com>
2021-08-13 18:30:50 +1000

    Display the response returned when adding a show

--- src/AddShow.js
+++ src/AddShow.js
@@ -6,18 +6,30 @@ import './AddShow.css';
 
 const AddShow = () => {
     const [showToAdd, setShowToAdd] = useState('')
+    const [result, setResult] = useState('')
 
     const addShowSubmission = event => {
         event.preventDefault()
-        requests.addShowToList(showToAdd).then(data => console.log(data)).catch(err => console.log(err.response))
+        requests.addShowToList(showToAdd).then(data => {
+            setResult(data.message)
+        }).catch(err => {
+            setResult(err.response.data.message)
+        })
+    }
+
+    const clearAddShowInput = () => {
+        setShowToAdd('')
+        setResult('')
     }
 
     return (
         <div id="add-show">
             <form onSubmit={event => addShowSubmission(event)}>
-                <input type="text" id="show-to-add" placeholder="Show name" onChange={event => setShowToAdd(event.target.value)}/>
+                <input type="text" id="show-to-add" placeholder="Show name" value={showToAdd} onChange={event => setShowToAdd(event.target.value)}/>
                 <input type="submit" id="submit-show" value={'Add '+showToAdd}/>
             </form>
+            <blockquote id="add-show-result">{result}</blockquote>
+            <button onClick={() => clearAddShowInput()}>OK</button>
         </div>
     )
 }
