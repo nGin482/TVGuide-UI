fd8673fc83ad6ca952245ae2e02c8c4d09d099e7
nGin482 <ngin2547@gmail.com>
2021-08-20 22:11:18 +1000

    Handling and styles for removing a show from the list

--- src/ShowList.js
+++ src/ShowList.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
+import Modal from 'react-modal';
 import requests from "./requests/requests.js";
 
 const ShowList = () => {
     const [showList, setShowList] = useState([])
+    const [result, setResult] = useState('')
+    const [openResultModal, setOpenResultModal] = useState(false)
 
     useEffect(() => {
         requests.getShowList().then(data => {
@@ -13,13 +16,27 @@ const ShowList = () => {
     }, []
     )
 
+    const deleteShowFromList = show => {
+        requests.removeShowFromList(show).then(data => {
+            setResult(data.message)
+            setOpenResultModal(true)
+        }).catch(response => {
+            setResult(response.data.message)
+        })
+    }
+
     return (
         <div id="list-of-shows">
             {showList.map(show => (
-                <div className="show" key={show}>
+                <div className="show-from-list" key={show}>
                     <blockquote className="show-header">{show}</blockquote>
+                    <button className="remove-show-from-list" onClick={() => deleteShowFromList(show)}>Delete {show}</button>
                 </div>
             ))}
+            <Modal isOpen={openResultModal}>
+                <button onClick={() => setOpenResultModal(false)}>Close</button>
+                <blockquote>{result}</blockquote>
+            </Modal>
         </div>
     )
 
