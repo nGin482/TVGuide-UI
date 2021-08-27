125c8e4a30eeb01ef9bec4a65ce5bd6ba0a9fcca
nGin482 <ngin2547@gmail.com>
2021-08-27 14:38:06 +1000

    Change to how reminder time will be displayed

--- src/ReminderCard.js
+++ src/ReminderCard.js
@@ -3,7 +3,7 @@ import './ReminderCard.css';
 const RemidnerCard = ({reminder}) => (
     <div className="reminder-card">
         <h4>{reminder.show}</h4>
-        <blockquote className="reminder time">{reminder['reminder time']}</blockquote>
+        <blockquote className="reminder time">You will be reminded {reminder['reminder time']} minutes {reminder['reminder alert'].toLowerCase()} an episode starts</blockquote>
         <blockquote className="interval">{reminder.interval}</blockquote>
     </div>
 )
