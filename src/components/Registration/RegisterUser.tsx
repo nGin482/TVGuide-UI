182de18d3103959e572d0eab83d0308ecf21c1c9
nGin482 <nicholasgiumelli@gmail.com>
2024-02-03 12:05:57 +1100

    TVGUI-12 Switching to AntD's form component

--- src/components/Registration/RegisterUser.tsx
+++ src/components/Registration/RegisterUser.tsx
@@ -1,11 +1,20 @@
-import React, { useState, FormEvent } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
+import { Form, Input, Select } from 'antd';
 
-import { registerNewUser } from '../../requests/requests';
+import { getRecordedShows, getReminders, registerNewUser } from '../../requests/requests';
+import { RecordedShowModel, Reminder } from '../../utils';
 import './RegisterUser.css';
 
 const RegisterUser = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [recordedShows, setRecordedShows] = useState<RecordedShowModel[]>([]);
+    const [reminders, setReminders] = useState<Reminder[]>([]);
+
+    useEffect(() => {
+        getRecordedShows().then(recordedShows => setRecordedShows(recordedShows));
+        getReminders().then(reminders => setReminders(reminders));
+    }, []);
     
     const registerUser = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -13,35 +22,51 @@ const RegisterUser = () => {
             username,
             password
         };
-        console.log(user)
         const response = await registerNewUser(user);
 
         if (response.result === 'success') {
-            console.log(response.message)
+            console.log(response)
         }
-        setUsername('');
-        setPassword('');
     };
     
     return (
         <div id='register-user'>
-            <form id='register-form' onSubmit={registerUser}>
-                <label htmlFor="username" id="username-label" className="register-label">Username</label>
-                <input
-                    type="text"
-                    id="register-username" className="register-input" name="username"
-                    value={username}
-                    onChange={event => setUsername(event.target.value)}
-                />
-                <label htmlFor="password" id="password-label" className="register-label">Password</label>
-                <input
-                    type="password"
-                    id="register-password" className="register-input" name="password"
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
-                />
-                <input type="submit" value="Register" id="submit-new-user"/>
-            </form>
+            <Form
+            
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Show Subscriptions"
+                    name="show_subscriptions"
+                >
+                    <Select
+                        options={recordedShows ? recordedShows.map(show => ({ label: show.show, value: show.show })) : []}
+                        showSearch
+                        notFoundContent='No Shows available'
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Reminder Subscriptions"
+                    name="reminder_subscriptions"
+                    >
+                    <Select
+                        options={reminders ? reminders.map(reminder => ({ label: reminder.show, value: reminder.show })) : []}
+                        showSearch
+                        notFoundContent='No Reminders available'
+                    />
+                </Form.Item>
+            </Form>
         </div>
     );
 };
