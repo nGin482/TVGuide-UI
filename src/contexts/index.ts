8c05abc1b6368068c8911da0fec27210192de27d
nGin482 <nicholasgiumelli@gmail.com>
2024-02-18 18:13:33 +1100

    TVGUI-13 User context

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,9 +1,11 @@
 import { RecordedShowsContext } from "./RecordedShowsContext";
 import { RemindersContext } from "./RemindersContext";
 import { SearchListContext } from "./SearchListContext";
+import { UserContext } from "./UserContext";
 
 export {
     RecordedShowsContext,
     RemindersContext,
-    SearchListContext
+    SearchListContext,
+    UserContext
 };
