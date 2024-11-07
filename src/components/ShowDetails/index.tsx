5e35187891e44baec1e0bf463152ecffc82d2e9f
nGin482 <nicholasgiumelli@gmail.com>
2024-11-07 21:06:44 +1100

    TVG-58 Styles for the show description

--- src/components/ShowDetails/index.tsx
+++ src/components/ShowDetails/index.tsx
@@ -1,33 +1,34 @@
 import { Image, List } from "antd";
 
 import type { ShowDetails } from "../../utils/types";
+import "./ShowDetails.css";
 
 
 interface ShowDetailsProps {
     showDetails: ShowDetails
 }
 
-const ShowDetails = ({ showDetails }: ShowDetailsProps) => {
-
-    return (
-        <>
-            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-                <Image src={showDetails.image} width="auto" height={500} />
-                <List
-                    dataSource={showDetails.genres}
-                    renderItem={item => (
-                        <List.Item>
-                            {item}
-                        </List.Item>
-                    )}
-                    style={{ width: "20%" }}
-                    bordered
-                    header={<h1>Genres</h1>}
-                />
-            </div>
-            <p dangerouslySetInnerHTML={{ __html: showDetails.description }} />
-        </>
-    );
-};
+const ShowDetails = ({ showDetails }: ShowDetailsProps) => (
+    <>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <Image src={showDetails.image} width="auto" height={500} />
+            <List
+                dataSource={showDetails.genres}
+                renderItem={item => (
+                    <List.Item>
+                        {item}
+                    </List.Item>
+                )}
+                bordered
+                header={<h1>Genres</h1>}
+                className="genres-list"
+            />
+        </div>
+        <div
+            dangerouslySetInnerHTML={{ __html: showDetails.description }}
+            className="show-description"
+        />
+    </>
+);
 
 export { ShowDetails };
