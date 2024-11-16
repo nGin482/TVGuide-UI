84ccb679aa74c77c74987173af38c65cd3faf782
nGin482 <nicholasgiumelli@gmail.com>
2024-11-16 13:01:27 +1100

    TVG-58 Add handling to switch view for show data

--- src/Pages/ShowPage.tsx
+++ src/Pages/ShowPage.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
-import { Alert } from "antd";
+import { NavLink, useLocation, useParams } from "react-router-dom";
+import { Alert, Button } from "antd";
 import { Helmet } from "react-helmet";
 
 import { ShowDetails } from "../components/ShowDetails";
@@ -9,14 +9,18 @@ import { SearchItem } from "../components/SearchItem";
 import { Reminder } from "../components/Reminders";
 import { RecordedShowsContext } from "../contexts";
 import { ShowData } from "../utils/types";
+import "./styles/ShowPage.css";
 
 interface ShowParam {
     show: string
 };
 
+type DataView = "episodes" | "search" | "reminder";
+
 const ShowPage = () => {
     const { show } = useParams<ShowParam>();
     const [showData, setshowData] = useState<ShowData>(null);
+    const [dataView, setDataView] = useState<DataView>("episodes");
 
     const { shows } = useContext(RecordedShowsContext);
     const location = useLocation();
@@ -26,6 +30,14 @@ const ShowPage = () => {
         setshowData(showDetail);
     }, [show, shows]);
 
+    const dataButtonClass = (view: DataView) => {
+        let className = "switch-view-button";
+        if (location.pathname.includes(view)) {
+            className += " active";
+        }
+        return className;
+    };
+
     return (
         showData ? (
             <div id={showData.show_name} className="show-content">
@@ -34,16 +46,46 @@ const ShowPage = () => {
                 </Helmet>
                 <h1>{showData.show_name}</h1>
                 <ShowDetails showDetails={showData.show_details} />
-                {location.pathname.includes("episodes") && (
+                <div className="show-data-switch">
+                    <NavLink to={`/shows/${showData.show_name}/episodes`}>
+                        <Button
+                            size="large"
+                            className={dataButtonClass("episodes")}
+                            onClick={() => setDataView("episodes")}
+                        >
+                            Episodes
+                        </Button>
+                    </NavLink>
+                    <NavLink to={`/shows/${showData.show_name}/search`}>
+                        <Button
+                            size="large"
+                            className={dataButtonClass("search")}
+                            onClick={() => setDataView("search")}
+                        >
+                            Search Criteria
+                        </Button>
+                    </NavLink>
+                    <NavLink to={`/shows/${showData.show_name}/reminder`}>
+                        <Button
+                            size="large"
+                            className={dataButtonClass("reminder")}
+                            onClick={() => setDataView("reminder")}
+                        >
+                            Reminder
+                        </Button>
+                    </NavLink>
+                </div>
+
+                {dataView === "episodes" && (
                     <ShowEpisodes
                         episodes={showData.show_episodes}
                         showName={showData.show_name}
                     />
                 )}
-                {location.pathname.includes("search") && (
+                {dataView === "search" && (
                     <SearchItem searchItem={showData.search_item} show={showData.show_name} />
                 )}
-                {location.pathname.includes("reminder") && (
+                {dataView === "reminder" && (
                     <Reminder reminder={showData.reminder} show={showData.show_name} />
                 )}
             </div>
